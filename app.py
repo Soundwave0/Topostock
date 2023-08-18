56a398711c9c161d24570fe2d61e7e90eaf9aa19
Soundwave0 <glenbrooktechservice@gmail.com>
2023-08-18 17:38:49 +0200

    fixed templates folder developed routing modified the hrefs in navbar for the pages

--- app.py
+++ app.py
@@ -4,4 +4,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('C:\\Users\\konch\\Downloads\\__pycache__\\templates\\home.html')
+    return render_template('home.html')
+@app.route('/usage')
+def usage():
+    return render_template('usage.html')
+@app.route('/listof')
+def listof():
+    return render_template('listof.html')
+
