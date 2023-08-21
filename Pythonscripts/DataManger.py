ed4df8fd7906c5733200ec80680ff991e8560b82
Soundwave0 <glenbrooktechservice@gmail.com>
2023-08-21 11:59:01 +0200

    Progress1

---
+++ Pythonscripts/DataManger.py
@@ -0,0 +1,10 @@
+import requests
+import json
+import pandas
+import numpy
+
+
+#get a stock API, receive data and get the nice bits
+#include method that makes the data look nice
+#turn data into nice numpy array
+#create a normalized array
