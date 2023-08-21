e050f3d75eebe7cf2412183f747bc94c60834935
Soundwave0 <glenbrooktechservice@gmail.com>
2023-08-21 11:48:43 +0200

    Progress2

---
+++ templates/scripts.js
@@ -0,0 +1,9 @@
+
+window.onload = function()
+{ 
+document.getElementById("stockButton").onclick = function()
+    {
+    var stockName = document.getElementById("stockName").value;
+    console.log(stockName);
+    }
+}
