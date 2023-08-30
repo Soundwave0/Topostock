93e9205bdf69f2ce27ef025f55e8353b0659d480
Soundwave0 <glenbrooktechservice@gmail.com>
2023-08-30 02:04:00 +0200

    Basic Algo and Clean-Up

--- Pythonscripts/TopologyDataFormatter.py
+++ Pythonscripts/TopologyDataFormatter.py
@@ -1,59 +1,35 @@
-
-#get topological data API
-#save the topological data in some format 
-#normalize the data and turn it into a data frame or an array of arrays
-#later implement some sort of algorithm that will find the most compatible array with
-#the StockData that is normalized
-#ideas for this
-# least squared simmilarities algorithm- the data points are compared and the difference squared
-#the sum of these differnce squared should be minimized, if there are 2 with the same score, choose the one with the lower index
-#use a neural network that trains off mountain z_score normalized data then goes on and tries the same thing with the stock z_score
-#normalized data
-
-#possible uses and functions 
-#https://github.com/opengeo-tech/GeoPicker
-#https://github.com/cbrum11/elevate/wiki
-
 import requests as r
 import pandas as pd
 import json
-import CalculationFunctions as CF
+from CalculationFunctions import z_score, normalize_array, calculate_ls_score,generate_random_coords
 import numpy as np
-import matplotlib.pyplot as plt
+
+
+def topology_data_normal(longDistance = 0.5, longIncrement = 0.015, numberoftrials =1):
+    latPre = []
+    longPre = []
+    
+    for trials in range(numberoftrials):
+        coords = generate_random_coords(1)
+        lat0 = round(coords[0][0],4)
+        lo0 = round(coords[0][1],4)
+        for k in np.arange(0,longDistance,longIncrement):
+            latPre.append(lat0)
+            longPre.append(lo0+k)
+
+    lat = ",".join(map(str,latPre))
+    long = ",".join(map(str,longPre))
+                
+    elevation_API = "https://api.open-meteo.com/v1/elevation?latitude={latitude}&longitude={longitude}"
+    elevationRequest = elevation_API.format(latitude=lat,longitude=long)
+    response = r.get(elevationRequest)
+    elevationDict = json.loads(response.text)
+    elevationArray = elevationDict['elevation']
+    elevationArrayNorm = normalize_array(elevationArray)
+    return elevationArrayNorm
 
 
 
-latPre = []
-longPre = []
-
-#for i in np.arange(30,32,0.25):
-#    for j in np.arange(81,82,0.25):
-i=30
-j=81
-for k in np.arange(0,0.25,0.01):
-    latPre.append(i)
-    longPre.append(j+k)
-
-
-lat = ",".join(map(str,latPre))
-long = ",".join(map(str,longPre))
-            
-elevation_API = "https://api.open-meteo.com/v1/elevation?latitude={latitude}&longitude={longitude}"
-elevationRequest = elevation_API.format(latitude=lat,longitude=long)
-print(elevationRequest)
-response = r.get(elevationRequest)
-#print(response)
-elevationDict = json.loads(response.text)
-elevationArray = elevationDict['elevation']
-print(elevationArray)
-elevationArray = CF.normalize_array(elevationArray)
-plt.plot(elevationArray)
-plt.show()
-
-
-
-
-#open file, then write these elevations
 
 
 
